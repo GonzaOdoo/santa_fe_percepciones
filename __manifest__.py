b8fdd0504a78450256dc88c29262bf9ebd8c377e
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-05-17 07:03:37 -0300

    Actualizadoa version de modulo

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0',
+    'version': '19.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['sale','base','account'],
