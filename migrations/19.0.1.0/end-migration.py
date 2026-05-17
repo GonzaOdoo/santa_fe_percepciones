8fa8873249679a4c227430297e16e74fe35034da
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-05-17 07:18:28 -0300

    Moved migration to end

--- migrations/19.0.1.0/end-migration.py
+++ migrations/19.0.1.0/end-migration.py
@@ -8,7 +8,8 @@ def migrate(cr, version):
     _logger.info( util.module_installed(cr, 'santa_fe_percepciones'))
     _logger.info('Checking if module account-payment-group is installed')
     _logger.info(util.module_installed(cr, 'account-payment-group'))
-    
+    _logger.info("Renaming module account-payment-group to account_payment_group")
+    util.rename_module(cr, 'account-payment-group', 'account_payment_group')
     _logger.info('Uninstalling module santa_fe_percepciones')
     util.uninstall_module(cr,'santa_fe_percepciones')
     
