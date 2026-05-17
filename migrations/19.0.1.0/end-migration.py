97bd1498a35c4482c629287e5a20d168c588c542
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-05-17 08:07:15 -0300

    removed rename of module

--- migrations/19.0.1.0/end-migration.py
+++ migrations/19.0.1.0/end-migration.py
@@ -9,7 +9,7 @@ def migrate(cr, version):
     _logger.info('Checking if module account-payment-group is installed')
     _logger.info(util.module_installed(cr, 'account-payment-group'))
     _logger.info("Renaming module account-payment-group to account_payment_group")
-    util.rename_module(cr, 'account-payment-group', 'account_payment_group')
+    #util.rename_module(cr, 'account-payment-group', 'account_payment_group')
     _logger.info('Uninstalling module santa_fe_percepciones')
     util.uninstall_module(cr,'santa_fe_percepciones')
     
