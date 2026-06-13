76a1290fb0c06064eea2e07d569815ebe4b52c31
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 23:29:40 -0300

    Removed script

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import sale_order
+#from . import sale_order
 from . import res_partner
 from . import import_padron_wizard
-from . import fiscal_position
-from . import account_payment
+#from . import fiscal_position
+#from . import account_payment
 #from . import account_tax
