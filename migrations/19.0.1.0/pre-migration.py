dca3cab4dbe4c0f7bde1f6f196f3ef05471cc58a
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 21:52:13 -0300

    Rollback

--- migrations/19.0.1.0/pre-migration.py
+++ migrations/19.0.1.0/pre-migration.py
@@ -19,10 +19,4 @@ def migrate(cr, version):
     """)
 
     _logger.warning("Forced legacy modules from 'to upgrade' to 'installed'")
-    cr.execute("""
-        UPDATE l10n_latam_check
-           SET payment_date = create_date::date
-         WHERE id = 1400
-           AND payment_date IS NULL
-    """)
-    _logger.warning("Set payment_date to create_date for l10n_latam_check with id 1400")
+   
