a9431c4fbcf9d04e39717a217b057abf01c22793
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 21:08:59 -0300

    Added migration script for modules

---
+++ migrations/19.0.1.0/pre-migration.py
@@ -0,0 +1,20 @@
+import logging
+from odoo.upgrade import util
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE ir_module_module
+           SET state = 'installed'
+         WHERE name IN (
+             'account-payment-group',
+             'account-payment-patch',
+             'l10n_ar_account_withholding',
+             'l10n_ar_withholding_ux',
+             'account_invoice_pricelist',
+             'Acccount_taxes',
+             'Res_partner'
+         )
+           AND state = 'to upgrade'
+    """)
+    _logger.warning("Forced legacy modules from 'to upgrade' to 'installed'")
