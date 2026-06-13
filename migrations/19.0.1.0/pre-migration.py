9fadc090e751bfe62d9b1d9a4135beb88b7e5355
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 23:31:40 -0300

    Updated migration script

--- migrations/19.0.1.0/pre-migration.py
+++ migrations/19.0.1.0/pre-migration.py
@@ -3,6 +3,26 @@ from odoo.upgrade import util
 _logger = logging.getLogger(__name__)
 
 def migrate(cr, version):
+
+    cr.execute("""
+        SELECT name, state
+        FROM ir_module_module
+        WHERE name IN (
+            'account-payment-group',
+            'account-payment-patch',
+            'l10n_ar_account_withholding',
+            'l10n_ar_withholding_ux',
+            'account_invoice_pricelist',
+            'Acccount_taxes',
+            'Res_partner'
+        )
+        ORDER BY name
+    """)
+    _logger.warning(
+        "Module states before fix: %s",
+        cr.fetchall()
+    )
+
     cr.execute("""
         UPDATE ir_module_module
            SET state = 'installed'
@@ -18,5 +38,26 @@ def migrate(cr, version):
            AND state = 'to upgrade'
     """)
 
-    _logger.warning("Forced legacy modules from 'to upgrade' to 'installed'")
-   
+    _logger.warning(
+        "Modules changed from 'to upgrade' to 'installed': %s",
+        cr.rowcount
+    )
+
+    cr.execute("""
+        SELECT name, state
+        FROM ir_module_module
+        WHERE name IN (
+            'account-payment-group',
+            'account-payment-patch',
+            'l10n_ar_account_withholding',
+            'l10n_ar_withholding_ux',
+            'account_invoice_pricelist',
+            'Acccount_taxes',
+            'Res_partner'
+        )
+        ORDER BY name
+    """)
+    _logger.warning(
+        "Module states after fix: %s",
+        cr.fetchall()
+    )
