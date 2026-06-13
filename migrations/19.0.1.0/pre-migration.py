e3e80910bb6740923b5f30ab9d3265185655012d
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-13 00:32:35 -0300

    Added installation script

--- migrations/19.0.1.0/pre-migration.py
+++ migrations/19.0.1.0/pre-migration.py
@@ -33,3 +33,35 @@ def migrate(cr, version):
         ORDER BY name
     """)
     _logger.warning("Migration module status: %s", cr.fetchall())
+
+    _logger.warning(
+        "Installing replacement modules"
+    )
+
+    util.force_install_module(
+        cr,
+        "account_payment_group",
+    )
+
+    util.force_install_module(
+        cr,
+        "l10n_ar_tax",
+    )
+    cr.execute("""
+        SELECT name, state
+        FROM ir_module_module
+        WHERE name IN (
+            'account_payment_group',
+            'l10n_ar_tax'
+        )
+    """)
+    _logger.warning(
+        "Replacement modules after force install: %s",
+        cr.fetchall()
+    )
+    if util.module_installed(cr, "account_payment_group"):
+        util.remove_module(cr, "account-payment-group")
+
+    if util.module_installed(cr, "l10n_ar_tax"):
+        util.remove_module(cr, "l10n_ar_account_withholding")
+        util.remove_module(cr, "l10n_ar_withholding_ux")
